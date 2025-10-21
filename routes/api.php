3801fffd1f55f45e4f73ee9490cef09b04a7bd8c
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-10-21 13:12:39 +0400

    Finish lesson Routes & Api

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\V1\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])
+->get('/user', function (Request $request) {
+    return $request->user();
+})->name('home');
+
+Route::prefix('v1')->group(function() {
+
+    Route::apiResource('posts', PostController::class);
+
+});
+
+
